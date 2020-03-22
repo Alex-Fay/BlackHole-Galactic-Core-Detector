733424cda9e232a84554d4063819efaeadb61522
Alex Fay <43191440+Alex-Fay@users.noreply.github.com>
2020-03-21 21:08:32 -0700

    range and saving file name changed

--- DataPre-Processing/imageRetrieval.py
+++ DataPre-Processing/imageRetrieval.py
@@ -1,8 +1,8 @@
-#@Author: Alex Fay 3/21/2020
-import urllib.request
+#@Author: Alex Fay 3/21/2020import urllib.request
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
+from google.colab import files
 
 #read csv file
 file = "Galaxy.csv"
@@ -10,10 +10,13 @@ df = pd.read_csv(file)
 
 #getting image from each row and column
 for i in df.index:
-  for i in range(2,-1):
+  for i in range(2,500002):
     ra = df['ra'][i]
     dec = df['dec'][i]
+    objID = df['objID'][i]
 
     url ="http://skyserver.sdss.org/dr16/SkyServerWS/ImgCutout/getjpeg?TaskName=Skyserver.Chart.List&ra=" + str(ra) + "&dec=" + str(dec)
-    imgName = str(i) + ".jpg"
+    imgName = "Gal+fileNum:" + str(i) + "+objID:" + str(objID) + ".jpg"
     urllib.request.urlretrieve(url, imgName)
+    print(i)
+    files.download(imgName)
