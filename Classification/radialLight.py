588dc7524803adc310f5ac65dd93adeb9d3c389b
Alex Fay <43191440+Alex-Fay@users.noreply.github.com>
2020-03-31 16:11:28 -0700

    added radial light plot

--- Classification/radialLight.py
+++ Classification/radialLight.py
@@ -1,14 +1,17 @@
+#@Alex Fay 3/30/2020
 from math import sqrt
 from PIL import Image
+import numpy as np
 
 #to get center take object and divide hieght and width by two
 center = [100, 200] #get from img
 width = 100 #get from img pixels
 height= 100 # get from img pix
+brightness = 0
 
 #todo add image cleaner?: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_photo/py_non_local_means/py_non_local_means.html
 
-#Get galaxy Light Profile
+#============= Get galaxy Light Profile =====================
 
 #convert to RGB image 
 #TODO change image names and make it work for all imgs
@@ -20,11 +23,13 @@ deltaH = height / 40
 deltaW = width /40
 radh = round(center[0] + deltaH)
 radw = round(center[1] + deltaW)
-prevBrightness = 0
+prevBrightness, trueBrightness, area = 0, 0, 0
+brightnessPlotList = np.array([brightness, area])
 
-#split into 20 points to plot
+  #split into 20 points to plot
 for i in range (0,20):
 
+  #go through each pixel and add brightness 
   for x in range(-radw, radw):
     for y in range (-radh, radh):
       pixelRGB = img.getpixel((x, y))
@@ -32,13 +37,22 @@ for i in range (0,20):
       luminanceVal = (.2126*R + .7152*G + .0722*B) #get luminance value
       brightness += luminanceVal
 
-  #plot point
-  
-  #radh = radh + deltah
-  #radw += deltaw
+  trueBrightness = brightness - prevBrightness # outercircle - inner circle
+  area = radh * radw
+  #update values for plotting and next circle
+  brigthnessPlotList = np.append(brightnessPlotList, (brightness, area))
+  radh += deltaH
+  radw += deltaW
+  i += 1
 
-  #TrueBrightness = brightness - radialBrightness
-  #add trueBrightness to array 
-  # i+1 and repeat
+  #plot point
+  import matplotlib
+  from matplotlib import pyplot as plt
+  plt.plot(brightnessPlotList)
+  plt.title("Brightness to Area")
+  plt.ylabel('Brightness')
+  plt.xlabel('Area')
+  plt.show()
 
 #Unsupervised learning algorithm
+
