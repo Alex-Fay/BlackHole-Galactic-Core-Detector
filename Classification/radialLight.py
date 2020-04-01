dacb5c2cb87514700f056eb278f3ea90209a329c
Alex Fay <43191440+Alex-Fay@users.noreply.github.com>
2020-04-01 01:27:17 -0700

    added plot and luminous values

--- Classification/radialLight.py
+++ Classification/radialLight.py
@@ -1,12 +1,12 @@
-#@Alex Fay 3/30/2020
+#@Alex Fay 3/30-1/2020
 from math import sqrt
 from PIL import Image
 import numpy as np
 
 #to get center take object and divide hieght and width by two
-center = [100, 200] #get from img
-width = 100 #get from img pixels
-height= 100 # get from img pix
+center = [800, 400] #get from img
+width = 1600 #get from img pixels
+height= 800 # get from img pix
 brightness = 0
 
 #todo add image cleaner?: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_photo/py_non_local_means/py_non_local_means.html
@@ -15,44 +15,53 @@ brightness = 0
 
 #convert to RGB image 
 #TODO change image names and make it work for all imgs
-img = Image.open("nibbler.jpg").convert('RGB')
-img.save('NibblerRGB.png')
+img = Image.open("galaxy.png").convert('RGB')
+img.save('GalRGB.png')
 
 # define change in rad and first rad
 deltaH = height / 40
 deltaW = width /40
-radh = round(center[0] + deltaH)
-radw = round(center[1] + deltaW)
+radh = int(center[0] + deltaH)
+negRadh = int(center[0] - deltaH)
+radw = int(center[1] + deltaW)
+negRadw = int(center[1] - deltaW)
 prevBrightness, trueBrightness, area = 0, 0, 0
-brightnessPlotList = np.array([brightness, area])
+luminousPlot = np.array(brightness)
+areaPlot = np.array(area)
 
-  #split into 20 points to plot
-for i in range (0,20):
+#split into 20 points to plot
+for i in range(0,20):
 
   #go through each pixel and add brightness 
-  for x in range(-radw, radw):
-    for y in range (-radh, radh):
+  for x in range(0, 485): #negRadw, radw                   #<=======TODO
+    for y in range(0, 624): #negRadh, radh
       pixelRGB = img.getpixel((x, y))
       R,G,B = pixelRGB
       luminanceVal = (.2126*R + .7152*G + .0722*B) #get luminance value
       brightness += luminanceVal
 
-  trueBrightness = brightness - prevBrightness # outercircle - inner circle
+  trueBrightness = round(brightness - prevBrightness) # outercircle - inner circle
   area = radh * radw
+
   #update values for plotting and next circle
-  brigthnessPlotList = np.append(brightnessPlotList, (brightness, area))
+  luminousPlot = np.append(luminousPlot, trueBrightness)
+  areaPlot = np.append(areaPlot, area)
   radh += deltaH
+  negRadh -= deltaH
   radw += deltaW
+  negRadw -= deltaW
   i += 1
 
-  #plot point
-  import matplotlib
-  from matplotlib import pyplot as plt
-  plt.plot(brightnessPlotList)
-  plt.title("Brightness to Area")
-  plt.ylabel('Brightness')
-  plt.xlabel('Area')
-  plt.show()
+print("BrightNess:", luminousPlot)
+print("Area", areaPlot)
+
+#plot point
+import matplotlib
+from matplotlib import pyplot as plt
+plt.plot(areaPlot, luminousPlot, 'r')
+plt.title("Brightness to Area")
+plt.ylabel('Brightness in luminance pixel magnitude')
+plt.xlabel('Area in pixels')
+plt.show()
 
 #Unsupervised learning algorithm
-
