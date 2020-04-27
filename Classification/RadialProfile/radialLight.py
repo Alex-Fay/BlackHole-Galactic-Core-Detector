8244c87de0e3b74a89a39ea43b2efbbc0a67c07e
Alex Fay <43191440+Alex-Fay@users.noreply.github.com>
2020-04-27 06:49:15 -0700

    added readcsv function and distant checker

--- Classification/RadialProfile/radialLight.py
+++ Classification/RadialProfile/radialLight.py
@@ -2,10 +2,17 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+import pandas as pd
 
+#Run file seperatly for each type of galaxy
 #======== Variables ========= 
 center = [60, 60]
-img = cv.imread("Brightness13-16100.jpg")
+readFile = "HubbleImageLabels.csv" #or other csv 
+df = pd.read_csv(readFile)
+imageCSV = df['ImgName']
+labelCol = df['Label']
+item = 0
 
 #=========== Radial Light Profile Function =============
 def radial_profile(img, center):
@@ -15,14 +22,26 @@ def radial_profile(img, center):
     tbin = np.bincount(r.ravel(), img.ravel()) #integrate pixel brightness with increasing r
     nr = np.bincount(r.ravel()) #get dr over length r
     radialprofile = tbin / nr #divide integral of light profile by r change
-    return radialprofile
+    predictRadial = np.flipud(radialprofile) #reverse array 
+    sumRad = np.sum(radialprofile)
+    return radialprofile, predictRadial, sumRad
+
+#====== Profile to CSV =======
+for item in df.index & range(1, 1):
+  noise = 5 #avg background space
+  img = cv.imread(imageCSV[i]) #TODO: change to full csv file
+  Y, predictRadial, sumRad = radial_profile(img, center)
+  if (sumRad > 1500): #ignore to distant galaxies
+    ndf = pd.DataFrame(list(zip(predictRadial)))
+    ndf.to_csv("example.csv")
+    item +=1
+  else: item +=1
 
 #============ plot points ===============
 import matplotlib
 from matplotlib import pyplot as plt
-Y = radial_profile(img, center)
 plt.plot(Y, 'r')
 plt.title("Image Luminosity vs. Radius")
 plt.ylabel('Luminance pixel magnitude')
-plt.xlabel('Radius in pixels')
+plt.xlabel('Radius from center')
 plt.show()
